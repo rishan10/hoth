92c610ca34d03aef875240c0004eec049a50877c
Rishan Girish <rishang10@gmail.com>
2019-12-22 18:26:13 -0800

    added navbar tabs and logo

--- src/pages/index.js
+++ src/pages/index.js
@@ -7,21 +7,24 @@ import SEO from "../components/seo"
 
 import Typical from 'react-typical'
 import "./index.css"
+import "../components/animate.css"
 
 
 const things = ["Leader.", 500, "Coder.", 500, "Musician.", 500, "Gamer.", 500, "Thinker.", 500]
 
 const IndexPage = () => (
 <div>
-  <Layout/>
+  <div class="animated fadeInLeft"><Layout/></div>
     <SEO title="Home" />
     <div class="center">
     <div class="intro_div">
+      <div class="animated zoomIn">
       <h1 class="intro_h1">Hi, I'm Rishan.</h1>
       <br></br>
       <p>
         I'm a <Typical loop={Infinity} steps={things} />
       </p>
+      </div>
     </div>
     </div>
     
