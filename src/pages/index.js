5ddbc525e8bc34c5cd32f0be086fd05f0e329282
Rishan Girish <rishang10@gmail.com>
2019-12-23 01:11:34 -0800

    particles

--- src/pages/index.js
+++ src/pages/index.js
@@ -13,12 +13,56 @@ import GitHubIcon from '@material-ui/icons/GitHub';
 import LinkedInIcon from '@material-ui/icons/LinkedIn';
 import EmailIcon from '@material-ui/icons/Email';
 
+import Particles from 'react-particles-js';
+
 
 const things = ["Leader.", 500, "Coder.", 500, "Musician.", 500, "Gamer.", 500, "Thinker.", 500]
 
+function getParticles() {
+  return (
+    <Particles 
+    params={{
+	    "particles": {
+	        "number": {
+	            "value": 50
+	        },
+	        "size": {
+              "random": true, 
+              "value": 10
+          },
+          "shape": {
+            "type": "polygon",
+            "stroke": {
+                "width": 2,
+                "color": "#F4B400",
+            },
+            "polygon": {
+                "nb_sides": Math.floor(Math.random() * 7) + 3
+            },
+          },
+          "line_linked": {
+            "color": "#F4B400",
+          }
+          
+	    },
+	    "interactivity": {
+	        "events": {
+	            "onhover": {
+	                "enable": true,
+	                "mode": "bubble",
+	            }
+	        }
+	    }
+	}} />
+  )
+}
+
 const IndexPage = () => (
-  <div>
+  <div class="landing">
     <div class="animated fadeInLeft"><Layout /></div>
+
+    {getParticles()}
+
     <SEO title="Home" />
     <div class="center">
       <div class="intro_div">
