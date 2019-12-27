f61d5fb9e7d4b90b6ae749c1d32f89d38f942c28
Rishan Girish <rishang10@gmail.com>
2019-12-26 21:44:53 -0800

    about typing

--- src/pages/about.js
+++ src/pages/about.js
@@ -4,6 +4,7 @@ import SEO from "../components/seo"
 import Typography from '@material-ui/core/Typography';
 import Divider from '@material-ui/core/Divider';
 import IconBar from '../components/iconbar';
+import Typing from 'react-typing-animation';   
 
 import "./about.css"
 
@@ -17,7 +18,9 @@ export default class About extends Component {
                 <Layout/>
 
                 <div class = "text">
+                    <Typing>
                 <Typography variant="h2">About Me</Typography>
+                </Typing>
                 <Divider/>
                 <Typography variant="paragraph">I'm a third year Computer Science student at UCLA. In the past, I have interned at Tala Security and Verizon Media and will be an incoming SDE intern at Honey.
                 I am mainly a backend developer with interests in AI/ML, distributed systems and cloud computing, and highly scalable systems and architectures.
@@ -25,7 +28,9 @@ export default class About extends Component {
                 Facebook and Taboola to organize recruiting events on campus. <br/><br/>
 
                 When I'm not coding, you can find me playing tennis -- a sport that has been with me since the tender age of
-                4. I also enjoy watching and playing soccer, singing, and gaming with friends.  
+                4. I also enjoy watching and playing soccer, singing, and gaming with friends. <br/><br/>
+
+                Let's get in touch! 
                 
                  </Typography>
                  <IconBar></IconBar>
